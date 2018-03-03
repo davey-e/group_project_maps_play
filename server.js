ab73addb76c6c4c01ea168ad4e5b1161eda814fb
Dave Ellis <dave@ellisnetmail.co.uk>
2018-03-03 21:55:56 +0000

    Another possible fix for Heroku app not loading

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
@@ -8,9 +8,11 @@ app.get('/', function (req, res) {
 
 app.use(express.static('public'));
 
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+const port = process.env.PORT || 3000;
+
+const server = app.listen(port, function () {
+  const host = server.address().address;
+  const port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
